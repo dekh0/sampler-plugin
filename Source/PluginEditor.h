12bbbbb66a9d9220ff33f493e84d64f4fa64689e
dekh0 <66974623+dekh0@users.noreply.github.com>
2022-04-10 19:13:33 +0400

    init

---
+++ Source/PluginEditor.h
@@ -0,0 +1,32 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class SamplerpluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    SamplerpluginAudioProcessorEditor (SamplerpluginAudioProcessor&);
+    ~SamplerpluginAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    juce::TextButton mLoadButton { "Load" };
+    SamplerpluginAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SamplerpluginAudioProcessorEditor)
+};
